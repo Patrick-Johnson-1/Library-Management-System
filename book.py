e9684a2bbbff2484dbefacbcb453cf171c626a79
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-03 17:36:01 -0500

    Getters Set

--- book.py
+++ book.py
@@ -5,4 +5,24 @@ class Book:
         self.__author = author
         self.__total_copies = total_copies
         self.__copies_available = total_copies
+
+    #Getters Set
+    @property
+    def isbn(self):
+        return self.__isbn
+    @property
+    def title(self):
+        return self.__title
+    @property
+    def author(self):
+        return self.__author
+    @property
+    def copies_available(self):
+        return self.__copies_available
+    @property
+    def total_copies(self):
+        return self.__total_copies
+
+
+
 book = Book("978-1-234", "1984", "George Orwell", 3)
