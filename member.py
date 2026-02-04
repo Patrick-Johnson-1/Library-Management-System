a41a443fc1f5c9484ccbef5e72c046b51aebe811
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-04 12:03:11 -0500

    added getters and setters. Fixed bug with borrow vs borrowed

--- member.py
+++ member.py
@@ -2,3 +2,22 @@ class Member:
     def __init__(self,member_id,name,borrow_limit=5):
         self.__member_id = member_id
         self.__name = name
+        self.__borrowed_books = []
+        self.__borrow_limit = borrow_limit
+
+    @property
+    def member_id(self):
+        return self.__member_id
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, value):
+        if len(value) > 0:
+            self.__name = value
+    @property
+    def borrowed_books(self):
+        return self.__borrowed_books.copy()
+    @property
+    def books_borrowed_count(self):
+        return len(self.__borrowed_books)
